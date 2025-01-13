0ae191e8d3a6dd4646720059703880935e3014ff
Arteronn <149862014+Arteronn@users.noreply.github.com>
2025-01-13 11:13:41 +0500

    Update SUCCession.py

--- SUCCession.py
+++ SUCCession.py
@@ -1,15 +1,15 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-403.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
 # ~ maxsum = 0
-# ~ minnum = 999999999
+# ~ minnum = 100001
 # ~ for i in A:
-	# ~ if A < minnum:
-		# ~ minnum = A
-# ~ for i in range(100000):
-	# ~ if A[i] % 18 == A[i] and A[i+1] % 18 == minnum:
+	# ~ if i < minnum:
+		# ~ minnum = i
+# ~ for i in range(len(A)-1):
+	# ~ if A[i] % 18 + A[i+1] % 18 == minnum:
 			# ~ t += 1
 			# ~ if A[i] + A[i+1] > maxsum:
 				# ~ maxsum = A[i] + A[i+1]
@@ -20,15 +20,15 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-404.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
-# ~ minsum = 999999999
-# ~ minnum = 999999999
+# ~ minsum = 200002
+# ~ minnum = 100001
 # ~ for i in A:
-	# ~ if A < minnum:
-		# ~ minnum = A
-# ~ for i in range(100000):
-	# ~ if A[i] % 55 == A[i] or A[i+1] % 55 == minnum:
+	# ~ if i < minnum:
+		# ~ minnum = i
+# ~ for i in range(len(A)-1):
+	# ~ if A[i] % 55 == minnum or A[i+1] % 55 == minnum:
 			# ~ t += 1
 			# ~ if A[i] + A[i+1] < minsum:
 				# ~ minsum = A[i] + A[i+1]
@@ -39,15 +39,15 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-403.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
 # ~ maxsum = 0
-# ~ minnum = 999999999
+# ~ minnum = 100001
 # ~ for i in A:
-	# ~ if A < minnum:
-		# ~ minnum = A
-# ~ for i in range(100000):
-	# ~ if A[i] % 65 == A[i] and A[i+1] % 65 == minnum:
+	# ~ if i < minnum:
+		# ~ minnum = i
+# ~ for i in range(len(A)-1):
+	# ~ if A[i] % 65 + A[i+1] % 65 == minnum:
 			# ~ t += 1
 			# ~ if A[i] + A[i+1] > maxsum:
 				# ~ maxsum = A[i] + A[i+1]
@@ -58,15 +58,15 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-403.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
-# ~ minpro = 999999999
-# ~ minnum = 999999999
+# ~ minpro = 1000000000
+# ~ minnum = 100001
 # ~ for i in A:
-	# ~ if A < minnum:
-		# ~ minnum = A
-# ~ for i in range(100000):
-	# ~ if A[i] % 77 * A[i+1] % 77 == minnum ** 2:
+	# ~ if i < minnum:
+		# ~ minnum = i
+# ~ for i in range(len(A)-1):
+	# ~ if (A[i] % 77) * (A[i+1] % 77) == minnum ** 2:
 			# ~ t += 1
 			# ~ if A[i] * A[i+1] < minpro:
 				# ~ minpro = A[i] * A[i+1]
@@ -77,14 +77,14 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-407.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
 # ~ td = 0
 # ~ maxsum = 0
 # ~ for i in A:
 	# ~ if i % 32 == 0:
 		# ~ td += 1
-# ~ for i in range(100000):
+# ~ for i in range(len(A)-1):
 	# ~ if (A[i] < 0 or A[i+1] < 0) and A[i] + A[i+1] < td:
 		# ~ t += 1
 		# ~ if A[i] + A[i+1] > maxsum:
@@ -96,7 +96,7 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-408.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
 # ~ maxsum = 0
 # ~ thr = 0
@@ -105,7 +105,7 @@
 # ~ for i in A:
 	# ~ if abs(i % 10) == 3 and abs(i) > 99 and abs(i) < 1000 and i > bigguy:
 		# ~ bigguy = i
-# ~ for i in range(100000):
+# ~ for i in range(len(A)-2):
 	# ~ thr = 0
 	# ~ tri = 0
 	# ~ for j in range(3):
@@ -125,7 +125,7 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-409.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
 # ~ maxsum = 0
 # ~ svn = 0
@@ -134,7 +134,7 @@
 # ~ for i in A:
 	# ~ if abs(i % 10) == 7 and abs(i) > 999 and abs(i) < 10000 and i > bigguy:
 		# ~ bigguy = i
-# ~ for i in range(100000):
+# ~ for i in range(len(A)-2):
 	# ~ svn = 0
 	# ~ qua = 0
 	# ~ for j in range(3):
@@ -154,17 +154,17 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-410.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
 # ~ maxsum = 0
-# ~ minnum = 999999999
+# ~ minnum = 100001
 # ~ for i in A:
-	# ~ if A < minnum:
-		# ~ minnum = A
-# ~ for i in range(100000):
-	# ~ if A[i] % 16 == A[i] or A[i+1] %  == minnum:
+	# ~ if i < minnum:
+		# ~ minnum = i
+# ~ for i in range(len(A)-1):
+	# ~ if A[i] % 16 == minnum or A[i+1] % 16 == minnum:
 			# ~ t += 1
-			# ~ if A[i] + A[i+1] < maxsum:
+			# ~ if A[i] + A[i+1] > maxsum:
 				# ~ maxsum = A[i] + A[i+1]
 # ~ print(t)
 # ~ print(maxsum)
@@ -173,16 +173,16 @@
 # ~ F = open("Z:/11-ПРОФИЛЬ/17data/17-426.txt")
 # ~ A = []
 # ~ for line in F:
-	# ~ A.append(line)
+	# ~ A.append(int(line))
 # ~ t = 0
-# ~ minsum = 99999999
+# ~ minsum = 3000000001
 # ~ thr = 0
 # ~ tri = 0
 # ~ bigguy = 0
 # ~ for i in A:
 	# ~ if abs(i % 100) == 43 and abs(i) > 9999 and abs(i) < 100000 and i > bigguy:
 		# ~ bigguy = i
-# ~ for i in range(100000):
+# ~ for i in range(len(A)-2):
 	# ~ thr = 0
 	# ~ tri = 0
 	# ~ for j in range(3):
@@ -196,4 +196,4 @@
 		# ~ if A[i] ** 2 + A[i+1] ** 2 + A[i+2] ** 2 < minsum:
 			# ~ minsum = A[i] ** 2 + A[i+1] ** 2 + A[i+2] ** 2
 # ~ print(t)
-# ~ print(maxsum)
+# ~ print(minsum)
